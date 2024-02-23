5b1db326b99f2a95e7a54a964f195e449c13dcfe
Otman Malki <otmanmalki57@gmail.com>
2024-02-23 12:16:54 +0100

    Initial commit

---
+++ src/main/java/com/example/movie_back/MovieBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.movie_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieBackApplication.class, args);
+    }
+
+}
